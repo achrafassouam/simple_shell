e7a5b0c245c1848c559570d4be65c1bb52871dcb
“name” <achrafassouam@gmail.com>
2023-12-13 18:26:54 -0800

    adding the first string functions file

--- string_functions1.c
+++ string_functions1.c
@@ -30,3 +30,97 @@ char *_strdup(char *str)
 	new[i] = '\0';
 	return (new);
 }
+
+/**
+ * *_strcpy - copy string fro src to dest
+ * @dest: char type input
+ * @src: char type input
+ * Return: dust.
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+/**
+ * *_strcat - concatenates two strings.
+ * @dest: char type pointer
+ * @src: char type pointer
+ * Return: dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	char *p = dest;
+
+	while (*p)
+		p++;
+	while (*src)
+	{
+		*p = *src;
+		p++;
+		src++;
+	}
+	*p = *src;
+	return (dest);
+}
+
+/**
+ * _itoa - int to ascii convertion
+ * @n: integer
+ *
+ * Return: ascii
+ */
+
+char *_itoa(int n)
+{
+	char buffer[20];
+	int i = 0;
+
+	if (n == 0)
+		buffer[i++] = '0';
+	else
+	{
+		while (n > 0)
+		{
+			buffer[i++] = (n % 10) + '0';
+			n /= 10;
+		}
+	}
+
+	buffer[i] = '\0';
+	_reverse(buffer, i);
+
+	return (_strdup(buffer));
+}
+
+/**
+ * _reverse - reverse a string
+ * @str: the string to reverse
+ * @len: length of the string
+ */
+
+void _reverse(char *str, int len)
+{
+	char tmp;
+	int start = 0;
+	int end = len - 1;
+
+	while (start < end)
+	{
+		tmp = str[start];
+		str[start] = str[end];
+		str[end] = tmp;
+		str++;
+		end--;
+	}
+}
