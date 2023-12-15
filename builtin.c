54f4edf88ef33499809280cb89b6ef7b0e124259
“name” <achrafassouam@gmail.com>
2023-12-15 15:08:06 -0800

    fixing the env builtin

--- builtin.c
+++ builtin.c
@@ -66,13 +66,15 @@ void exit_builtin(char **inpt, int *stats)
 
 void env_builtin(char **inpt, int *stats)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; environ[i]; i++)
+	while (environ[i] != NULL)
 	{
-		write(STDOUT_FILENO, environ[i], _strlen(environ[i]));
-		write(STDERR_FILENO, "\n", 1);
+		write(1, environ[i], _strlen(environ[i]));
+		write(1, "\n", 1);
+		i++;
 	}
+
 	free_adv(inpt);
 	*stats = 0;
 }
